7e7090b3f62f640412fe69c42bd2d3c04491eaa5
Nathaniel Risum <nathaniel.risum2@gmail.com>
2024-09-25 22:54:47 +0200

    Add DOCUMENTATION for MC props

--- TicketClassLibrary/MC.cs
+++ TicketClassLibrary/MC.cs
@@ -6,20 +6,35 @@ using System.Threading.Tasks;
 
 namespace TicketClassLibrary
 {
+    /// <summary>
+    /// MC vehicle class. Nothing is static, so class has to be instantiated to be used.
+    /// </summary>
     public class MC
     {
-
+        /// <summary>
+        /// Method to return price of the instance.
+        /// </summary>
+        /// <returns>Price of the instance.</returns>
         public double Price()
         {
             return 125; 
         }
-
+        /// <returns>Price of the instance</returns>/// <summary>
+        /// Method to return the type of the vehicle.
+        /// </summary>
+        /// <returns>A string: "MC".</returns>
         public string Vehicle()
         {
             return "MC"; 
         }
 
+        /// <summary>
+        /// Property of the Licenseplate of the Car
+        /// </summary>
         public string Licenseplate {  get; set; }
+        /// <summary>
+        /// Property of the Date of the Car
+        /// </summary>
         public DateTime date { get; set; }
 
     }
